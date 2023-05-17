e0b2dd0fc477a4c63721405285d38ab1eac39495
ravigunz <ravigunz@gmail.com>
2023-05-18 00:57:22 +0530

    left menu item align

--- src/layouts/homelLayout/LeftMenu.js
+++ src/layouts/homelLayout/LeftMenu.js
@@ -6,7 +6,7 @@ export default function LeftMenu() {
     <div>
       <Grid container>
         <Grid item xs={12} sx={{ bgcolor: "#313B54", p: 2 }}>
-          <Typography sx={{ color: "white" }}>
+          <Typography align="left" sx={{ color: "white" }}>
             <Chip label="&copy;" sx={{ bgcolor: "blue", color: "white" }} />{" "}
             CORE<b>UI </b>
             <Chip
@@ -17,7 +17,7 @@ export default function LeftMenu() {
           </Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#45536D", p: 1 }}>
-          <Typography sx={{ color: "white" }}>
+          <Typography align="left" sx={{ color: "white" }}>
             DASHBOARD{" "}
             <Chip
               label="New"
@@ -27,58 +27,58 @@ export default function LeftMenu() {
           </Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 4 }}>
-          <Typography sx={{ color: "white" }}>THEME</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 2 }}>THEME</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Colors</Typography>
-        </Grid>
-        <Grid item xs={12} sx={{ bgcolor: "#3B4B64", p: 1 }}>
-          <Typography sx={{ color: "white" }}>Typography</Typography>
-        </Grid>
-        <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 4, pb: 2 }}>
-          <Typography sx={{ color: "white" }}>COMPONENTS</Typography>
+          <Typography align="left" sx={{ color: "white", pl:4 }}>Colors</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Base</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Typography</Typography>
+        </Grid>
+        <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 4 }}>
+        <Typography align="left" sx={{ color: "white", pl: 2 }}>COMPONENTS</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Buttons</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Base</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Charts</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Buttons</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Editors</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Charts</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Forms</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Editors</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Google Maps</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Forms</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Icons</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Google Maps</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Notification</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Icons</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Plugins</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Notification</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Tables</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Plugins</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
-          <Typography sx={{ color: "white" }}>Widgets</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Tables</Typography>
         </Grid>
-        <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 4, pb: 1 }}>
-          <Typography sx={{ color: "white" }}>EXTRAS</Typography>
+        <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1 }}>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Widgets</Typography>
         </Grid>
-        <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 0, pb: 1 }}>
-          <Typography sx={{ color: "white" }}>Pages</Typography>
+        <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 4 }}>
+          <Typography align="left" sx={{ color: "white", pl: 2 }}>EXTRAS</Typography>
+        </Grid>
+        <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pt: 1, pb: 1 }}>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Pages</Typography>
         </Grid>
         <Grid item xs={12} sx={{ bgcolor: "#3B4B64", pb: 2 }}>
-          <Typography sx={{ color: "white" }}>Apps</Typography>
+          <Typography align="left" sx={{ color: "white", pl: 4 }}>Apps</Typography>
         </Grid>
       </Grid>
     </div>
